a2a05934f372f22d694eb8bb69e84dc4ebf2df04
Siwik <michalsiwik@gmail.com>
2019-04-15 12:00:22 +0200

    Zmiana algorytmu użycie str_replace_all_fixed z pakietu stringi, baaaaaaaaaaaaaardzo przyspieszyło pracę na bardzo dużych zbiorach (okolo 10 000 razy szybciej)

--- R/usuwaniePolskichZnakow.R
+++ R/usuwaniePolskichZnakow.R
@@ -7,31 +7,9 @@
 #' @export
 #'
 #'
-usuwaniePolskichZnakow <- Vectorize(function(x, perl = F) {
-
-  if (!is.character(x)) {
-    stop(sprintf(
-      "x is non-character but class: %s",
-      class(x))
-    )
-  }
-  x <- gsub(x, pattern = "Ą", replacement = "A", perl = perl)
-  x <- gsub(x, pattern = "Ć", replacement = "C", perl = perl)
-  x <- gsub(x, pattern = "Ę", replacement = "E", perl = perl)
-  x <- gsub(x, pattern = "Ł", replacement = "L", perl = perl)
-  x <- gsub(x, pattern = "Ś", replacement = "S", perl = perl)
-  x <- gsub(x, pattern = "Ń", replacement = "N", perl = perl)
-  x <- gsub(x, pattern = "Ó", replacement = "O", perl = perl)
-  x <- gsub(x, pattern = "Ź", replacement = "Z", perl = perl)
-  x <- gsub(x, pattern = "Ż", replacement = "Z", perl = perl)
-  x <- gsub(x, pattern = "ą", replacement = "a", perl = perl)
-  x <- gsub(x, pattern = "ć", replacement = "c", perl = perl)
-  x <- gsub(x, pattern = "ę", replacement = "e", perl = perl)
-  x <- gsub(x, pattern = "ł", replacement = "l", perl = perl)
-  x <- gsub(x, pattern = "ś", replacement = "s", perl = perl)
-  x <- gsub(x, pattern = "ń", replacement = "n", perl = perl)
-  x <- gsub(x, pattern = "ó", replacement = "o", perl = perl)
-  x <- gsub(x, pattern = "ź", replacement = "z", perl = perl)
-  x <- gsub(x, pattern = "ż", replacement = "z", perl = perl)
-  return(x)
-}, vectorize.args = "x")
+usuwaniePolskichZnakow_stri_replace_VEC_VERSION <- function(x) {
+  return(stri_replace_all_fixed(x,
+                                pattern =     c("Ą", "Ć", "Ę", "Ł", "Ś", "Ń", "Ó", "Ź", "Ż", "ą", "ć", "ę", "ł", "ś", "ń", "ó", "ź", "ż"),
+                                replacement = c("A", "C", "E", "L", "S", "N", "O", "Z", "Z", "a", "c", "e", "l", "s", "n", "o", "z", "z" ),
+                                vectorize_all=TRUE))
+}
