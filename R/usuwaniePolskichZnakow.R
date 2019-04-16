31d428aac60ad05679e5e4a247a77aff72b55f26
Siwik <michalsiwik@gmail.com>
2019-04-16 15:57:54 +0200

    Usunięcie zbędnych spacji i zmaian parametru na vectorize_all -> FALSE

--- R/usuwaniePolskichZnakow.R
+++ R/usuwaniePolskichZnakow.R
@@ -7,9 +7,9 @@
 #' @export
 #'
 #'
-usuwaniePolskichZnakow_stri_replace_VEC_VERSION <- function(x) {
+usuwaniePolskichZnakow <- function(x) {
   return(stri_replace_all_fixed(x,
-                                pattern =     c("Ą", "Ć", "Ę", "Ł", "Ś", "Ń", "Ó", "Ź", "Ż", "ą", "ć", "ę", "ł", "ś", "ń", "ó", "ź", "ż"),
+                                pattern = c("Ą", "Ć", "Ę", "Ł", "Ś", "Ń", "Ó", "Ź", "Ż", "ą", "ć", "ę", "ł", "ś", "ń", "ó", "ź", "ż"),
                                 replacement = c("A", "C", "E", "L", "S", "N", "O", "Z", "Z", "a", "c", "e", "l", "s", "n", "o", "z", "z" ),
-                                vectorize_all=TRUE))
+                                vectorize_all=FALSE))
 }
