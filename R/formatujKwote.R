1ac3b1b13eb9c4c1af2949be5926b27511e9fa34
Siwik <michalsiwik@gmail.com>
2019-07-19 15:04:18 +0200

    Dodana obsługa miliardów (mld)

--- R/formatujKwote.R
+++ R/formatujKwote.R
@@ -1,11 +1,11 @@
 #' Słuzy do formatowania kwot przy prezentacji na np. wykresach i danych zawsze zwraca character
 #'
 #' @param x vector numeryczny (numeric vector)
-#' @param miejscaPoPrzecinku ilosc miejsc po przecinku, ktore maja byc prezentowane domyclnie 1 (jedno)
+#' @param miejscaPoPrzecinku ilosc miejsc po przecinku, ktore maja byc prezentowane domyslnie 1 (jedno)
 #' @param prefix co ma byc przed kwota np. znak '$' domyslnie nic ''
 #' @param suffix co ma byc po kwocie np. znak '$' domyslnie nic 'zł'
 #'
-#' @return zwraca characet vector dla kwot
+#' @return zwraca character vector dla kwot
 #'
 #' @export
 #'
@@ -22,8 +22,10 @@ formatujKwote <- Vectorize(function(x, miejscaPoPrzecinku = 1, prefix = "", suff
     return(paste0(prefix, round(x, miejscaPoPrzecinku), suffix))
   } else if (abs(x) < 1000 * 1000) {
     return(paste0(prefix, round(x/1000, miejscaPoPrzecinku),"tys ", suffix))
-  } else {
+  } else if (abs(x) < 1000 * 1000 * 1000) {
     return(paste0(prefix, round(x/(1000*1000), miejscaPoPrzecinku),"mln ", suffix))
+  } else {
+    return(paste0(prefix, round(x/(1000*1000*1000), miejscaPoPrzecinku),"mld ", suffix))
   }
 }, vectorize.args = "x", USE.NAMES = F)
 
