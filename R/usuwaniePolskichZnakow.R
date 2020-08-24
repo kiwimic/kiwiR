b23b3895e08dff4b3f552da2f0d55c8098e89207
Michał Siwik (epruf) <michal.siwik@epruf.pl>
2020-08-24 15:08:40 +0200

    Moved patterns and replacements from arguments to body of funcion

--- R/usuwaniePolskichZnakow.R
+++ R/usuwaniePolskichZnakow.R
@@ -1,54 +1,55 @@
 #' Słuzy do zamiany polskich liter na ich odpowiedniki bez sladczkow
 #'
-#' @param x vector numeryczny (numeric vector)
+#' @param x character vector; strings to search in
 #'
 #' @return zwraca vector po usunieciu wszystkich polskich znakow (zamienieniu ich na odpowiednik a z sladczkiem -> a)
 #'
 #' @export
 #'
 #'
-usuwaniePolskichZnakow <- function(x,
-                                   pattern = c("\U0104", #Ą
-                                               "\U0106", #Ć
-                                               "\U0118", #Ę
-                                               "\U0141", #Ł
-                                               "\U015A", #Ś
-                                               "\U0143", #Ń
-                                               "\U00D3", #Ó
-                                               "\U0179", #Ź
-                                               "\U017B", #Ż
-                                               "\U0105", #ą
-                                               "\U0107", #ć
-                                               "\U0119", #ę
-                                               "\U0142", #ł
-                                               "\U015B", #ś
-                                               "\U0144", #ń
-                                               "\U00F3", #ó
-                                               "\U017A", #ź
-                                               "\U017C"),#ż
-                                   replacement = c("A",
-                                                   "C",
-                                                   "E",
-                                                   "L",
-                                                   "S",
-                                                   "N",
-                                                   "O",
-                                                   "Z",
-                                                   "Z",
-                                                   "a",
-                                                   "c",
-                                                   "e",
-                                                   "l",
-                                                   "s",
-                                                   "n",
-                                                   "o",
-                                                   "z",
-                                                   "z")) {
+usuwaniePolskichZnakow <- function(x) {
   return(
     stringi::stri_replace_all_fixed(
       x,
-      pattern = pattern,
-      replacement =  replacement,
+      pattern =  c(
+        "\U0104",        #Ą
+        "\U0106",        #Ć
+        "\U0118",        #Ę
+        "\U0141",        #Ł
+        "\U015A",        #Ś
+        "\U0143",        #Ń
+        "\U00D3",        #Ó
+        "\U0179",        #Ź
+        "\U017B",        #Ż
+        "\U0105",        #ą
+        "\U0107",        #ć
+        "\U0119",        #ę
+        "\U0142",        #ł
+        "\U015B",        #ś
+        "\U0144",        #ń
+        "\U00F3",        #ó
+        "\U017A",        #ź
+        "\U017C"),      #ż,
+      replacement  = c(
+        "A",
+        "C",
+        "E",
+        "L",
+        "S",
+        "N",
+        "O",
+        "Z",
+        "Z",
+        "a",
+        "c",
+        "e",
+        "l",
+        "s",
+        "n",
+        "o",
+        "z",
+        "z"
+      ),
       vectorize_all = FALSE
     )
   )
